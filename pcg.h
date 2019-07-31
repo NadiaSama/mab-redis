ee8e2319862cd9617a28ba06de2e5edbb8cad2ec
eeiwant <eeiwant@gmail.com>
2019-07-31 12:03:44 +0800

    1.add license info for pcg.h

--- pcg.h
+++ pcg.h
@@ -1,6 +1,8 @@
 #ifndef PCG_H
 #define PCG_H
 
+// *Really* minimal PCG32 code / (c) 2014 M.E. O'Neill / pcg-random.org
+// Licensed under Apache License 2.0 (NO WARRANTY, etc. see website)
 #include <stdint.h>
 
 
